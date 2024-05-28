3eb20011555f1acb00a791b1114a825aa9449b69
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-05-28 09:59:37 +0100

    feat: add postgres

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use newsletter::configuration::get_configuration;
 use newsletter::run;
 
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let address = TcpListener::bind(address)?;
+    run(address, connection_pool)?.await
 }

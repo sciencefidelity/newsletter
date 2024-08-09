a724ff18e13f50c89b6f6d55168fe52b2715d3ca
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-08-09 21:25:09 +0100

    fix: lints

--- flake.nix
+++ flake.nix
@@ -19,7 +19,9 @@
       {
         devShells.default = mkShell {
           buildInputs = [
+            docker-client
             pkg-config
+            postgresql
             sqlx-cli
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-analyzer" "rust-src" ];
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -65,7 +65,7 @@ impl TestApp {
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
         reqwest::Client::new()
-            .post(&format!("{}/newsletters", &app.address))
+            .post(&format!("{}/newsletters", &self.address))
             .json(&body)
             .send()
             .await
